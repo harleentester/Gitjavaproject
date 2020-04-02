b8a63790f5bca3fdff81f926768b8db4b8f7771d
harleentester <thind.harleen@gmail.com>
2020-04-02 12:18:51 -0400

    changes for today

--- src/main/java/git1.java
+++ src/main/java/git1.java
@@ -1,5 +1,7 @@
 public class git1 {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("Adding a line");
+        System.out.println("todays change");
     }
 }
